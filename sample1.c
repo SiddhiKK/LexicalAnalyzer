f52ba297ff773c68b3e10ad9597026ae11e1f322
SiddhiKK <siddhi.k1894@gmail.com>
2018-09-02 21:08:40 -0400

    Add files via upload

---
+++ sample1.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+	double x,y;
+	float s1,r=2.5,i,2a;
+	/*Calculating simple interest
+	with formula pnr/100 */
+	char c;
+	int p=100,n=3;
+	c=i*5;
+	//scanf("%d",&a);
+	"abc'	
+	s1=p*n*r/100;
+	printf("Simple Interest is %f",s1);
+	for(i=0;i<10;i++)
+	{
+		if(i%2==0)
+		{  
+			printf("\n HELLO");
+		}
+		else
+		{  
+			printf("\t WORLD");
+		}
+	}
+}
